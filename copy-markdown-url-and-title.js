66002236461165533b5fdd56062230b74aa97612
Jakub Chodorowicz <chodorowicz@gmail.com>
2023-05-11 12:01:47 +0200

    Change the output mode to compact
    Signed-off-by: Jakub Chodorowicz <chodorowicz@gmail.com>

--- copy-markdown-url-and-title.js
+++ copy-markdown-url-and-title.js
@@ -3,7 +3,7 @@
 // Required parameters:
 // @raycast.schemaVersion 1
 // @raycast.title Copy Markdown URL and Title
-// @raycast.mode fullOutput
+// @raycast.mode compact
 //
 // Optional parameters:
 // @raycast.packageName Copy Markdown URL and Title
